a86dc97b6adb2df9bea1c3016e3c77af859640b8
deazuri <deazuriraihan@gmail.com>
2022-10-31 18:19:59 +0700

    Praktikum Bab 4

---
+++ app/src/main/java/com/example/tugaspraktikum4/NeozoneFragment.java
@@ -0,0 +1,24 @@
+package com.example.tugaspraktikum4;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NeozoneFragment extends Fragment {
+
+    public NeozoneFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_neozone,container,false);
+    }
+}
